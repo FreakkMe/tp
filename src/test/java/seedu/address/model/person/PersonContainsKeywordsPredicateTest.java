645aff8e197b4343514cc39562b30d5363458e2d
SSHiroy <shiroysattur.m@gmail.com>
2026-03-19 22:17:49 +0800

    test: delete outdated test after feature update

--- src/test/java/seedu/address/model/person/PersonContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/PersonContainsKeywordsPredicateTest.java
@@ -72,11 +72,6 @@ public class PersonContainsKeywordsPredicateTest {
         // Non-matching keyword
         predicate = new PersonContainsKeywordsPredicate(Arrays.asList("Carol"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
-
-        // Keywords match phone, email and address, but does not match name
-        predicate = new PersonContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
-        assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345")
-                .withEmail("alice@email.com").withAddress("Main Street").build()));
     }
 
     @Test
